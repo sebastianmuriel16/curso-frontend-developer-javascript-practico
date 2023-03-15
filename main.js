b07f7e05d61fed1f781a2c73dbd530316e0bec55
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2023-03-15 17:55:32 -0500

    primeros pasos de integracion en las clases

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuEmail = document.querySelector(".navbar-email");
+const desktoMenu = document.querySelector(".desktop-menu");
+menuEmail.addEventListener('click', toggleDesktopMenu)
+
+function toggleDesktopMenu(){
+    desktoMenu.classList.toggle("inactive"); 
+}
