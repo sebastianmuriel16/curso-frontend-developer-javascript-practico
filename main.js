a474210c6e3137803b2bd3d0a1632e681368e439
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2023-03-17 16:03:03 -0500

    menu mobile

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktoMenu = document.querySelector(".desktop-menu");
-menuEmail.addEventListener('click', toggleDesktopMenu)
+const MenuHamIcon = document.querySelector(".menu"); // menu hamburgesa de la vista movil
+const Mobilemenu = document.querySelector(".mobile-menu");
+
+
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+MenuHamIcon.addEventListener('click', toggleMobileMenu);
+
+
 
 function toggleDesktopMenu(){
-    desktoMenu.classList.toggle("inactive"); 
+    desktoMenu.classList.toggle("inactive"); //toggle -> intercambiar entre quitar o agregar la clase inactive
+}
+
+function toggleMobileMenu(){
+    Mobilemenu.classList.toggle("inactive")
 }
